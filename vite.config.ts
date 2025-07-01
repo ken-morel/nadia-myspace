987d834588c47a6c489b632717e86e51edd66346
ken-morel <engonken8@gmail.com>
2025-07-02 00:29:52 +0100

    corrected rbs.cm in domain list in ...

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		paraglideVitePlugin({
-			project: './project.inlang',
-			outdir: './src/lib/paraglide'
-		})
-	],
-	preview: {
-		allowedHosts: ['ins-web-production.up.railway.app', 'ins.brs.cm']
-	}
+  plugins: [
+    sveltekit(),
+    paraglideVitePlugin({
+      project: './project.inlang',
+      outdir: './src/lib/paraglide'
+    })
+  ],
+  preview: {
+    allowedHosts: ['ins-web-production.up.railway.app', 'ins.rbs.cm']
+  }
 });
