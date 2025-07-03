78634ff5deacde329fe7e9015299b6f1eab619ae
ken-morel <engonken8@gmail.com>
2025-07-04 00:04:38 +0100

    Added sass and w3.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,24 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="w3-container w3-padding hero">
+	<h1><span>Bridging gaps,</span> <span>Building futures</span></h1>
+</div>
+
+<style lang="sass">
+$hh1: #909EAE
+$hh2: #5C8DC5
+$hh3: #AD9E90
+$hh4: #736F60
+.hero
+  background-color: lighten($hh1, 35)
+  margin: 0
+  padding: 40px
+  h1
+    margin: 50px
+    text-align: center
+    font-size: xxx-large
+    font: "Ubuntu mono"
+    font-weight: bold
+    span:first-child
+      color: #dd6666
+    span:last-child
+      color: #6666dd
+</style>
